99b9850c6dc7efa3945309e1ba64b6aa32fbee4b
Supermax95 <wallmax@mail.ru>
2023-09-22 16:58:30 +0700

    0.01

--- DolgovGroup/App.js
+++ DolgovGroup/App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
+
+// const Post = styled.View`
+//   padding: 15px;
+//   background-color: black;
+//   height: 80px;
+//   width: 80px;
+//   border-radius: 30px;
+// `;
+const PostImage = styled.Image`
+padding: 15px;
+height: 240px;
+width: 240px;
+resizeMode: contain;
+`;
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Добро пожаловать</Text>
+      < PostImage source={{uri:'https://poisk-firm.ru/storage/employer/logo/70/ba/a9/abb46e24b581abb40de2b12ed1.jpg'}}/>
+      <StatusBar theme="auto" />
     </View>
   );
 }
@@ -13,7 +30,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
