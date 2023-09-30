d1beef525690e3dbf86a81af29dcc12bd9c68c81
AleArtiu <rbirbrty0@gmail.com>
2023-09-30 16:02:50 +0300

    Кнопка окрашивается

--- App/src/components/SignIn/SignIn.tsx
+++ App/src/components/SignIn/SignIn.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useState } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Button from 'ui/Button';
 import Field from 'ui/Field';
 
 interface IData {
@@ -7,23 +8,32 @@ interface IData {
   password: string;
 }
 
+const styleCenter = 'h-full w-full bg-white pt-16';
+
 const SignIn: FC = () => {
   const [data, setData] = useState<IData>({} as IData);
+  const authHandler = () => {};
 
   return (
-    <View className="flex-1">
+    <View className={styleCenter}>
       <View className="mx-5 justify-center items-center h-full">
-        <Field
-          value={data.email}
-          placeholder="Введите email"
-          onChange={(value) => setData({ ...data, email: value })}
-        />
-        <Field
-          value={data.password}
-          placeholder="Введите пароль"
-          onChange={(value) => setData({ ...data, password: value })}
-          isSecure={true}
-        />
+        <Text className="text-center text-gray-800 text-2xl font-bold mb-2">
+          Авторизация
+        </Text>
+        <View className="w-9/12">
+          <Field
+            value={data.email}
+            placeholder="Введите email"
+            onChange={(value) => setData({ ...data, email: value })}
+          />
+          <Field
+            value={data.password}
+            placeholder="Введите пароль"
+            onChange={(value) => setData({ ...data, password: value })}
+            isSecure={true}
+          />
+          <Button onPress={authHandler} title={`Войти`} />
+        </View>
       </View>
     </View>
   );
