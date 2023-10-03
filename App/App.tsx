503d43c5b953e3ce95bda5986841b8bdc3039192
AleArtiu <rbirbrty0@gmail.com>
2023-10-03 21:17:34 +0300

    Кривой навбар нижний

--- App/App.tsx
+++ App/App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native';
-import { AppNavigator } from './src/navigation/Navigation';
+import { AppNavigator, FooterTabs } from './src/navigation/Navigation';
 import { NavigationContainer } from '@react-navigation/native';
+
+//import { useNavigationContainerRef } from '@react-navigation/native';
+
+// export default function App() {
+//   // const refNavigate = useNavigationContainerRef();
+
+//   return (
+//     <>
+//       {/* <NavigationContainer ref={refNavigate}> */}
+//       <NavigationContainer>
+//         <SafeAreaView className="flex-1">
+//           <AppNavigator />
+//           <FooterTabs />
+
+//           <StatusBar style="auto" />
+//         </SafeAreaView>
+//       </NavigationContainer>
+//       {/* <Footer navigate={refNavigate.navigate} /> */}
+//     </>
+//   );
+// }
 
 export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView className="flex-1">
+      <SafeAreaView style={{ flex: 1 }}>
         <AppNavigator />
         <StatusBar style="auto" />
       </SafeAreaView>
---
+++ App/src/navigation/types.ts
@@ -0,0 +1,8 @@
+export type TypeRootStackParamList = {
+  Auth: 'Вход' | undefined;
+  Home: 'Главная' | undefined;
+  Catalog: 'Каталог' | undefined;
+  Shops: 'Магазины' | undefined;
+  Support: 'Помощь' | undefined;
+  Profile: 'Профиль' | undefined;
+};
