e66a5e9d464acebb9d9b9923de33a98c4d036e1b
AleArtiu <rbirbrty0@gmail.com>
2023-10-09 22:37:30 +0300

    logout work

--- App/src/screens/Catalog/Catalog.tsx
+++ App/src/screens/Catalog/Catalog.tsx
@@ -1,34 +1,12 @@
-// import { View, Text } from 'react-native';
-// import React from 'react';
-
-// const Catalog = () => {
-//   return (
-//     <View>
-//       <Text>Catalog</Text>
-//     </View>
-//   );
-// };
-
-// export default Catalog;
-
 import { View, Text } from 'react-native';
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import userLogout from 'Redux/thunks/User/logout.api';
-import Button from 'ui/Button';
-import { useNavigation } from '@react-navigation/native';
 
-export default Catalog = () => {
-  const navigation = useNavigation();
-  const dispatch = useDispatch();
-
-  const handleLogout = () => {
-    dispatch(userLogout());
-  };
+const Catalog = () => {
   return (
     <View>
-      <Text>Profile</Text>
-      <Button onPress={handleLogout} title={`Выйти`} />
+      <Text>Catalog</Text>
     </View>
   );
 };
+
+export default Catalog;
