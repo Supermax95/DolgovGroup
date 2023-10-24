30658fa9b3ede6d5322e5b8e7c36262bf768b96b
Supermax95 <wallmax@mail.ru>
2023-10-24 22:09:09 +0700

    App.tsx

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,44 +1,15 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import { VITE_URL } from './VITE_URL';
 
-
 function App() {
-  const [data, setData] = useState({});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-      
-        const response = await fetch(`${VITE_URL}/api`); 
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>{data.message}</p>
-      <p>{data.someOtherData}</p>
+      <Routes>
+      {/* <Route path="/about" element={<AboutCompany />} /> */}
+        <Route />
+      </Routes>
     </>
   );
 }
