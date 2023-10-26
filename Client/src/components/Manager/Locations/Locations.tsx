47f6b4a1ad9db789102d1efe3200dc5d3f81390a
Supermax95 <wallmax@mail.ru>
2023-10-27 00:51:02 +0700

    CoolSidebar

--- Client/src/components/Manager/Locations/Locations.tsx
+++ Client/src/components/Manager/Locations/Locations.tsx
@@ -6,7 +6,6 @@ import editLocation from '../../../Redux/thunks/Locations/editLocation.api';
 import Sidebar from '../../Sidebar/Sidebar';
 import Wrapper from '../../../ui/Wrapper';
 
-
 interface Location {
   id: number;
   city: string;
@@ -31,6 +30,7 @@ const Location: React.FC = () => {
   const [editedLocation, setEditedLocation] = useState<
     Location | null | undefined
   >(null);
+  const [selectedCity, setSelectedCity] = useState<string | null>(null);
 
   useEffect(() => {
     dispatch(getLocations());
@@ -40,7 +40,11 @@ const Location: React.FC = () => {
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
 
-  const displayedLocations = locations.slice(startIndex, endIndex);
+  const filteredLocations = selectedCity
+    ? locations.filter((location) => location.city === selectedCity)
+    : locations;
+
+  const displayedLocations = filteredLocations.slice(startIndex, endIndex);
 
   const openEditModal = (location: Location) => {
     setSelectedLocation(location);
@@ -84,126 +88,126 @@ const Location: React.FC = () => {
     }
   };
 
-  const totalPages = Math.ceil(locations.length / itemsPerPage);
+  const totalPages = Math.ceil(filteredLocations.length / itemsPerPage);
   const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 
-  const handlePageChange = (page: React.SetStateAction<number>) => {
-    setCurrentPage(page);
-  };
+  // Извлекаем уникальные города
+  const uniqueCities = [...new Set(locations.map((location) => location.city))];
 
   return (
     <Wrapper>
-           <div className="p-4 ">
-    <div className="flex">
-      <Sidebar menuItems={locations} onMenuItemClick={openEditModal} />
       <div className="p-4">
-        <h1 className="text-2xl font-bold mb-4">Список магазинов</h1>
-        <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
-          <div className="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard rounded-bl-lg rounded-br-lg">
-            <table className="min-w-full">
-              <thead>
-                <tr>
-                  <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
-                    #
-                  </th>
-                  <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
-                    Город
-                  </th>
-                  <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
-                    Адрес
-                  </th>
-                  <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
-                    Широта
-                  </th>
-                  <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
-                    Долгота
-                  </th>
-                  <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
-                    Рабочее время
-                  </th>
-                  <th className="px-6 py-3 border-b-2 border-gray-300">
-                    <button
-                      onClick={openAddModal}
-                      className="py-2 px-10 rounded bg-green-500 text-white"
-                    >
-                      Добавить
-                    </button>
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white">
-                {displayedLocations.map((location, index) => (
-                  <tr key={location.id}>
-                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-                      {(currentPage - 1) * itemsPerPage + index + 1}
-                    </td>
-                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-                      <h2 className="text-xl font-semibold">{location.city}</h2>
-                    </td>
-                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-                      <p className="text-gray-600">{location.address}</p>
-                    </td>
-                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-                      <p className="text-gray-600">{location.latitude}</p>
-                    </td>
-                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-                      <p className="text-gray-600">{location.longitude}</p>
-                    </td>
-                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
-                      <p className="text-gray-600"> {location.hours}</p>
-                    </td>
-                    <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500">
-                      <button
-                        onClick={() => openEditModal(location)}
+        <div className="flex">
+          <Sidebar
+            menuItems={uniqueCities}
+            onMenuItemClick={setSelectedCity}
+            title="Города"
+          />
 
-                        className="bg-blue-500 hover-bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+          <div className="p-4">
+            <h1 className="text-2xl font-bold mb-4">Список магазинов</h1>
+            <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
+              <div className="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard rounded-bl-lg rounded-br-lg">
+                <table className="min-w-full">
+                  <thead>
+                    <tr>
+                      <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
+                        #
+                      </th>
+                      <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
+                        Город
+                      </th>
+                      <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
+                        Адрес
+                      </th>
+                      <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
+                        Широта
+                      </th>
+                      <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
+                        Долгота
+                      </th>
+                      <th className="px-6 py-3 border-b-2 border-gray-300 text-left leading-4 text-blue-500 tracking-wider">
+                        Рабочее время
+                      </th>
+                      <th className="px-6 py-3 border-b-2 border-gray-300">
+                        <button
+                          onClick={openAddModal}
+                          className="py-2 px-10 rounded bg-green-500 text-white"
+                        >
+                          Добавить
+                        </button>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white">
+                    {displayedLocations.map((location, index) => (
+                      <tr key={location.id}>
+                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                          {(currentPage - 1) * itemsPerPage + index + 1}
+                        </td>
+                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                          <h2 className="text-xl font-semibold">
+                            {location.city}
+                          </h2>
+                        </td>
+                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                          <p className="text-gray-600">{location.address}</p>
+                        </td>
+                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                          <p className="text-gray-600">{location.latitude}</p>
+                        </td>
+                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                          <p className="text-gray-600">{location.longitude}</p>
+                        </td>
+                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-500">
+                          <p className="text-gray-600"> {location.hours}</p>
+                        </td>
+                        <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-500">
+                          <button
+                            onClick={() => openEditModal(location)}
+                            className="bg-blue-500 hover-bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+                          >
+                            Редактировать
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
 
-                       
+            <div className="flex space-x-2 mt-4">
+              {pageNumbers.map((page) => (
+                <button
+                  key={page}
+                  onClick={() => setCurrentPage(page)}
+                  className={`px-3 py-2 rounded ${
+                    page === currentPage
+                      ? 'bg-blue-500 text-white'
+                      : 'bg-white text-blue-500 hover-bg-blue-200'
+                  }`}
+                >
+                  {page}
+                </button>
+              ))}
+            </div>
 
-                      >
-                        Редактировать
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            {isModalOpen && (selectedLocation || isAddingMode) && (
+              <LocationsModal
+                isOpen={isModalOpen}
+                location={selectedLocation}
+                onSave={handleSave}
+                onClose={closeEditModal}
+                isAddingMode={isAddingMode}
+                editedLocation={editedLocation}
+                setEditedLocation={setEditedLocation}
+              />
+            )}
           </div>
         </div>
-
-        <div className="flex space-x-2 mt-4">
-          {pageNumbers.map((page) => (
-            <button
-              key={page}
-              onClick={() => handlePageChange(page)}
-              className={`px-3 py-2 rounded ${
-                page === currentPage
-                  ? 'bg-blue-500 text-white'
-                  : 'bg-white text-blue-500 hover-bg-blue-200'
-              }`}
-            >
-              {page}
-            </button>
-          ))}
-        </div>
-
-        {isModalOpen && (selectedLocation || isAddingMode) && (
-          <LocationsModal
-            isOpen={isModalOpen}
-            location={selectedLocation}
-            onSave={handleSave}
-            onClose={closeEditModal}
-            isAddingMode={isAddingMode}
-            editedLocation={editedLocation}
-            setEditedLocation={setEditedLocation}
-          />
-        )}
       </div>
-
-    </div>
-
     </Wrapper>
-
   );
 };
 
--- Client/src/components/Sidebar/Sidebar.tsx
+++ Client/src/components/Sidebar/Sidebar.tsx
@@ -1,29 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface SidebarProps {
-  menuItems: { label: string }[];
-  onMenuItemClick: (label: string) => void;
+  menuItems: string[];
+  onMenuItemClick: (selectedCity: string) => void;
+  title: string;
+
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ menuItems, onMenuItemClick }) => {
+const Sidebar: React.FC<SidebarProps> = ({ menuItems, onMenuItemClick, title,
+     }) => {
+  const [selectedCity, setSelectedCity] = useState('Все магазины');
+  const [isCityListVisible, setCityListVisible] = useState(false);
+
+  const handleCityChange = (city: string) => {
+    if (city === 'all') {
+      setSelectedCity('Все магазины');
+      setCityListVisible(!isCityListVisible); // Открываем или скрываем список городов при выборе "Все магазины"
+      onMenuItemClick(''); // Сбрасываем фильтры
+    } else {
+      setSelectedCity(city);
+      onMenuItemClick(city);
+    }
+  };
+
   return (
     <div className="flex flex-col w-64 bg-white h-full border-r">
       <div className="h-16 flex items-center justify-center border-b">
-        <div className="text-xl font-bold">Sidebar</div>
+        <div className="text-xl font-bold">{title}</div>
       </div>
       <div className="flex-1 overflow-y-auto">
         <ul className="py-4 space-y-1">
-          {menuItems.map((item, index) => (
-            <li key={index}>
-              <button
-                className="flex items-center w-full h-12 px-4 text-gray-700 hover:text-indigo-500 focus:outline-none focus:bg-gray-100"
-                onClick={() => onMenuItemClick(item.label)}
-              >
-                <span className="mr-4"></span>
-                <span className="text-sm">{item.label}</span>
-              </button>
-            </li>
-          ))}
+          <li>
+            <button
+              className="flex items-center w-full h-12 px-4 text-gray-700 hover-text-indigo-500 focus:outline-none focus-bg-gray-100"
+              onClick={() => {
+                handleCityChange('all');
+              }}
+            >
+              <span className={`mr-4 ${isCityListVisible ? 'transform rotate-90' : ''} transition-transform`}>▶</span>
+              <span className="text-sm">Все магазины</span>
+            </button>
+          </li>
+          {isCityListVisible && (
+            menuItems.map((city, index) => (
+              <li key={index}>
+                <button
+                  className={`flex items-center w-full h-12 px-4 text-gray-700 hover-text-indigo-500 focus:outline-none focus-bg-gray-100 ${
+                    selectedCity === city ? 'bg-gray-100' : ''
+                  }`}
+                  onClick={() => handleCityChange(city)}
+                >
+                  <span className="mr-4"></span>
+                  <span className="text-sm">{city}</span>
+                </button>
+              </li>
+            ))
+          )}
         </ul>
       </div>
     </div>
