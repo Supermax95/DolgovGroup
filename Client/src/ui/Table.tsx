08f695eb41ceecb6af02d5941b2cf8fe30103399
AleArtiu <rbirbrty0@gmail.com>
2023-10-27 19:44:10 +0300

    таблица закончена

--- Client/src/ui/Table.tsx
+++ Client/src/ui/Table.tsx
@@ -1,113 +1,40 @@
-// import React, { FC } from 'react';
-// import Button from './Button';
-
-// interface ITable {
-//   nameTable: string;
-//   columnsName: any[];
-//   childrenButtonAdd: React.ReactNode;
-//   data: any[];
-//   columns: any[];
-//   childrenButtonEdit?: React.ReactNode;
-// }
-
-// const Table: FC<ITable> = ({
-//   nameTable,
-//   columnsName,
-//   childrenButtonAdd,
-//   data,
-//   columns,
-//   childrenButtonEdit,
-// }) => {
-//   return (
-//     <div>
-//       <h1 className="text-xl text-lime-600 font-medium mb-4">{nameTable}</h1>
-//       <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
-//         <div className="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard rounded-bl-lg rounded-br-lg">
-//           <table className="min-w-full">
-//             <thead>
-//               <tr>
-//                 {columnsName.map((columnName) => (
-//                   <th
-//                     key={columnName}
-//                     className="w-40 px-6 py-3 border-b-2 border-orange-300 text-left leading-4 text-slate-700 text-sm font-bold tracking-wider"
-//                   >
-//                     {columnName}
-//                   </th>
-//                 ))}
-//                 <th className=" border-b-2 border-orange-300">
-//                   {childrenButtonAdd}
-//                 </th>
-//               </tr>
-//             </thead>
-
-//             <tbody className="bg-white">
-//               {data.map((value, index) => (
-//                 <tr key={value}>
-//                   <td className="text-center border-b border-slate-400 text-slate-600 text-sm font-normal">
-//                     {index + 1}
-//                   </td>
-//                   {columns.slice(1).map((columnName) => (
-//                     <td
-//                       key={columnName}
-//                       className="px-6 py-4 whitespace-no-wrap border-b border-slate-400 text-slate-600 text-sm font-normal"
-//                     >
-//                       {value[columnName]}
-//                     </td>
-//                   ))}
-//                   <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-slate-400">
-//                     {/* <Button
-//                       type="button"
-//                       onClick={() => onEditClick(value)}
-//                       styleCSSButton={`relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-normal text-slate-600 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200`}
-//                       title="Редактировать"
-//                     /> */}
-//                     {childrenButtonEdit}
-//                   </td>
-//                 </tr>
-//               ))}
-//             </tbody>
-//           </table>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Table;
 import React, { FC } from 'react';
 import Button from './Button';
 
 interface ITable {
-  nameTable: string;
-  columns: { columnName: string }[];
+  title: string;
+  columnsDefaultName: { name: string }[];
   data: any[];
-  columnsToShow: string[];
+  columnsListDb: string[];
   onAddClick: () => void;
   onEditClick: (item: any) => void;
 }
 
 const Table: FC<ITable> = ({
-  nameTable,
-  columns,
-  columnsToShow,
+  title,
+  columnsDefaultName,
+  columnsListDb,
   data,
   onAddClick,
   onEditClick,
 }) => {
   return (
     <div>
-      <h1 className="text-xl text-lime-600 font-medium mb-4">{nameTable}</h1>
+      <h1 className="text-xl text-lime-600 font-medium mb-4">{title}</h1>
       <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 pr-10 lg:px-8">
         <div className="align-middle inline-block min-w-full shadow overflow-hidden bg-white shadow-dashboard rounded-bl-lg rounded-br-lg">
           <table className="min-w-full">
             <thead>
               <tr>
-                {columns.map((column, index) => (
+                <th className="w-16 pl-6 py-3 border-b-2 border-orange-300 text-left leading-4 text-slate-700 text-sm font-bold ">
+                  №
+                </th>
+                {columnsDefaultName.map((columnDefaultName, index) => (
                   <th
-                    key={index + column.columnName}
+                    key={index + columnDefaultName.name}
                     className="w-40 px-6 py-3 border-b-2 border-orange-300 text-left leading-4 text-slate-700 text-sm font-bold tracking-wider"
                   >
-                    {column.columnName}
+                    {columnDefaultName.name}
                   </th>
                 ))}
                 <th className="w-36 border-b-2 border-orange-300">
@@ -126,13 +53,13 @@ const Table: FC<ITable> = ({
             <tbody className="bg-white">
               {data.map((item, index) => (
                 <tr key={item.id}>
-                  <td className="text-center border-b border-slate-400 text-slate-600 text-sm font-normal">
+                  <td className="w-16 text-left pl-6 py-0 whitespace-no-wrap border-b border-slate-400 text-slate-600 text-sm font-normal pr-[-25px]">
                     {index + 1}
                   </td>
-                  {columnsToShow.slice(1).map((columnName) => (
+                  {columnsListDb.slice(1).map((columnName) => (
                     <td
                       key={columnName}
-                      className="px-6 py-4 whitespace-no-wrap border-b border-slate-400 text-slate-600 text-sm font-normal"
+                      className="px-4 py-4 whitespace-no-wrap border-b border-slate-400 text-slate-600 text-sm font-normal"
                     >
                       {item[columnName]}
                     </td>
