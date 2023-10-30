2dfa325e4b9b032ed35e151c4b380232a8fdd861
AleArtiu <rbirbrty0@gmail.com>
2023-10-30 19:09:37 +0300

    добавлена типизация на logout-web/native и check-web

--- App/src/Redux/thunks/User/logout.api.ts
+++ App/src/Redux/thunks/User/logout.api.ts
@@ -3,19 +3,26 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { PORT, IP } from '@env';
 
-export const userLogout = createAsyncThunk('api/logout', async () => {
-  try {
-    const response: AxiosResponse = await axios.post(
-      `http://${IP}:${PORT}/api/logout`,
-      null,
-      {
-        withCredentials: true,
-      }
-    );
-    return response.data;
-  } catch (error) {
-    throw error;
+interface ResponseData {
+  message: string;
+}
+
+export const userLogout = createAsyncThunk<ResponseData, void>(
+  'api/logout',
+  async () => {
+    try {
+      const response: AxiosResponse = await axios.post(
+        `http://${IP}:${PORT}/api/logout`,
+        null,
+        {
+          withCredentials: true,
+        }
+      );
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
   }
-});
+);
 
 export default userLogout;
--- Client/src/Redux/thunks/PortalLogin/portalLogout.api.ts
+++ Client/src/Redux/thunks/PortalLogin/portalLogout.api.ts
@@ -3,17 +3,24 @@ import type { AxiosResponse } from 'axios';
 import axios from 'axios';
 import { VITE_URL } from '../../../VITE_URL';
 
-const portalLogout = createAsyncThunk('api/logout', async () => {
-  const response: AxiosResponse = await axios.post(
-    `${VITE_URL}/portal/logout`,
-    {},
-    {
-      withCredentials: true,
-    }
-  );
-  console.log('response logout', response.data);
+interface ResponseData {
+  message: string;
+}
 
-  return response.data;
-});
+const portalLogout = createAsyncThunk<ResponseData, void>(
+  'api/logout',
+  async () => {
+    const response: AxiosResponse = await axios.post(
+      `${VITE_URL}/portal/logout`,
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+    console.log('response logout', response.data);
+
+    return response.data;
+  }
+);
 
 export default portalLogout;
