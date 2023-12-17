64468033366787ee1190feb284c679b329d9619f
AleArtiu <rbirbrty0@gmail.com>
2023-12-17 23:20:59 +0300

    исправлен чутка сайдбар

--- Client/src/ui/SidebarPromotionUI.tsx
+++ Client/src/ui/SidebarPromotionUI.tsx
@@ -1,4 +1,4 @@
-import  { FC } from 'react';
+import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
 interface IPromotionSidebarArray {
@@ -16,23 +16,23 @@ const PromotionSidebarUI: FC<IPromotionsSidebar> = ({ links, title }) => {
   return (
     <div className="flex flex-col w-64 bg-white h-full border-r-2 border-orange-300">
       <div className="h-12 flex items-center justify-center border-b-2 border-orange-300">
-      <h2 className="text-lg font-bold text-slate-600">{title}</h2>
-        </div>
-        <ul className="pt-2 pb-2 space-y-1 text-sm">
-          {links.map((item) => (
-            <li key={item.id}>
-              <Link
-                to={item.href}
-                className="flex items-center p-2 space-x-3 rounded-md hover:bg-slate-100"
-              >
-                <span className="text-slate-600 text-sm font-normal">
-                  {item.name}
-                </span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <h2 className="text-lg font-bold text-slate-600">{title}</h2>
       </div>
+      <ul className="pt-2 pb-2 space-y-1">
+        {links.map((item) => (
+          <li key={item.id} className="flex items-center">
+            <Link
+              to={item.href}
+              className="flex justify-between items-center p-2 rounded-md hover:bg-slate-100"
+            >
+              <span className="text-slate-600 text-sm font-normal">
+                {item.name}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
