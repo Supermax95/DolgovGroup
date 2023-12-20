f05246d79ccd6b8707e2e10bf1e29fd150e91656
Supermax95 <wallmax@mail.ru>
2023-12-20 21:24:57 +0700

    Editor

--- Client/src/components/Admin/Laws/Editor.tsx
+++ Client/src/components/Admin/Laws/Editor.tsx
@@ -1,226 +1,12 @@
-// import React, { ChangeEvent, FC, useEffect, useState } from 'react';
-// import { useAppDispatch, useAppSelector } from '../../../Redux/hooks';
-// import deleteProduct from '../../../Redux/thunks/Products/deleteProduct.api';
-// import Wrapper from '../../../ui/Wrapper';
-// import { ILaw } from './Laws';
-// import { VITE_URL } from '../../../VITE_URL';
-// import axios from 'axios';
-// import 'react-quill/dist/quill.snow.css'; 
-// import ReactQuill from 'react-quill';
-
-// interface LawEditorProps {
-//   isOpen: boolean;
-//   law: ILaw | null;
-//   onSaveAdd: (editedLaw: ILaw) => void;
-//   onSaveEdit:  (editedLaw: ILaw) => void;
-//   onCloseAddEditor: () => void;
-//   onCloseEditEditor: () => void;
-//   isAddingMode: boolean;
-//   editedLaw: ILaw | null | undefined;
-//   setEditedLaw: React.Dispatch<
-//     React.SetStateAction<ILaw | null | undefined>
-//   >;
-//   axiosError: string | null;
-//   resetAxiosError: () => void;
-// }
-
-// const Editor: FC<LawEditorProps> = ({
-//   isOpen,
-//   law,
-//   onSaveEdit,
-//   onSaveAdd,
-//   onCloseAddEditor,
-//   onCloseEditEditor,
-//   isAddingMode,
-//   editedLaw,
-//   setEditedLaw,
-//   axiosError,
-//   resetAxiosError,
-// }) => {
-//   const id = useAppSelector((state) => state.lawsSlice.postId);
-//   const dispatch = useAppDispatch();
-//   const [isUpload, setUpload] = useState(false);
-//   const [currentStep, setCurrentStep] = useState(1);
-
-//   useEffect(() => {
-//     if (law) {
-//       setEditedLaw({ ...law });
-//     }
-//   }, [law, isAddingMode, setEditedLaw]);
-//   const EditorTitle = isAddingMode ? 'Новый документ' : 'Редактирование документа';
-//   const handleCancel = () => {
-//     setEditedLaw(undefined);
-//     resetAxiosError();
-//     onCloseEditEditor();
-//     onCloseAddEditor();
-//   };
-
-//   const uploadFile = async (
-//     file: File,
-//     id: number | 0,
-//     isAddingMode: boolean
-//   ): Promise<void> => {
-//     if (file && id) {
-//       const formData = new FormData();
-//       formData.append('file', file);
-
-//       try {
-//         await axios.put(`${VITE_URL}/admin/documentFile/${id}`, formData, {
-//           headers: {
-//             'Content-Type': 'multipart/form-data',
-//           },
-//           withCredentials: true,
-//         });
-
-//         if (isAddingMode) {
-//             onCloseAddEditor();
-//         } else {
-//             onCloseEditEditor();
-//         }
-//       } catch (error) {
-//         console.error('Ошибка при загрузке файла:', error);
-//       }
-//     }
-//   };
-
-//   const handleFormSubmit = async (e: React.FormEvent) => {
-//     e.preventDefault();
-//     if (currentStep === 1) {
-//       let result = '';
-//       let result2 = '';
-
-//       if (isAddingMode) {
-//         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//         // @ts-ignore
-//         result = await onSaveAdd(editedLaw as ILaw);
-//       } else {
-//         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//         // @ts-ignore
-//         result2 = await onSaveEdit(editedLaw as ILaw);
-//       }
-
-//       if (typeof result2 !== 'string') {
-//         setCurrentStep(2);
-//         setUpload(true);
-//       }
-//       if (typeof result !== 'string') {
-//         setCurrentStep(2);
-//         setUpload(true);
-//       }
-//     } else if (currentStep === 2) {
-//       const fileInput = document.getElementById(
-//         'fileInput'
-//       ) as HTMLInputElement;
-//       const file = fileInput?.files?.[0];
-
-//       await uploadFile(
-//         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//         // @ts-ignore
-//         file,
-//         id,
-//         isAddingMode
-//       );
-//     }
-//   };
-
-//   const handleFileInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
-//     const file = e.target.files?.[0] || null;
-
-//     uploadFile(
-//       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//       // @ts-ignore
-//       file,
-//       id,
-//       isAddingMode
-//     );
-//   };
-
-//   const handleDelete = () => {
-//     if (editedLaw && editedLaw.id) {
-//       const productId = editedLaw.id;
-//       dispatch(deleteProduct(productId));
-//       onCloseEditEditor();
-//     }
-//   };
-
-//   if (!isOpen || !editedLaw) {
-//     return null;
-//   }
-
-// return (
-//     <Wrapper>
-//     <form onSubmit={handleFormSubmit}>
-//           {currentStep === 1 && (
-//             <div className="flex space-x-2 items-center justify-between pb-4">
-//               <div className="flex space-x-2">
-//                 <h1 className="text-slate-600 text-sm tracking-normal leading-tight">
-//                   Новый
-//                 </h1>
-//                 <ReactQuill
-//                 id="description"
-//                 theme="snow"
-//                 value={editedLaw.description}
-//                 onChange={(value) =>
-//                   setEditedProduct({ ...editedLaw, description: value })
-//                 }
-//                 // placeholder="Описание продукта"
-//                 className="w-full" /* Чтобы растягиваться по ширине контейнера */
-//               />
-//             </div>
-//           )}
-
-    
-//           {currentStep === 2 && (
-//             <>
-//               <div className="container mx-auto mt-8 p-8 max-w-4xl justify-center items-center flex-col block rounded-lg bg-white shadow-m">
-//                 <div className="px-4 sm:px-0 text-center">
-//                   <h1 className="text-xl font-bold mb-4">
-//                     Форма загрузки документа
-//                   </h1>
-//                   <span className="block mt-2 text-sm text-gray-500">
-//                     Если документ менять не нужно, вы можете
-//                     пропустить этот шаг
-//                   </span>
-//                   <div className="mt-6">
-//                     <div className="mb-4">
-//                       <input
-//                         type="file"
-//                         id="fileInput"
-//                         name="productPhoto"
-//                         className="hidden"
-//                         onChange={handleFileInputChange}
-//                       />
-//                       <label
-//                         htmlFor="fileInput"
-//                         className="cursor-pointer bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-//                       >
-//                         Выберите файл
-//                       </label>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             </>
-//           )}
-//       </form>
-//     </Wrapper>
-//   );
-// };
-
-// export default Editor;
-
-
-
-
 import React, { ChangeEvent, FC, useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../Redux/hooks';
-import deleteProduct from '../../../Redux/thunks/Products/deleteProduct.api';
 import Wrapper from '../../../ui/Wrapper';
 import { ILaw } from './Laws';
 import { VITE_URL } from '../../../VITE_URL';
 import axios from 'axios';
 import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
+import deleteLaw from '../../../Redux/thunks/Document/deleteLaw.api';
 
 interface LawEditorProps {
   isOpen: boolean;
@@ -299,22 +85,17 @@ const Editor: FC<LawEditorProps> = ({
     e.preventDefault();
     if (currentStep === 1) {
       let result = '';
-      let result2 = '';
 
       if (isAddingMode) {
-           //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         result = await onSaveAdd(editedLaw as ILaw);
       } else {
-                   //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        result2 = await onSaveEdit(editedLaw as ILaw);
+        result = await onSaveEdit(editedLaw as ILaw);
       }
 
-      if (typeof result2 !== 'string') {
-        setCurrentStep(2);
-        setUpload(true);
-      }
       if (typeof result !== 'string') {
         setCurrentStep(2);
         setUpload(true);
@@ -324,23 +105,23 @@ const Editor: FC<LawEditorProps> = ({
         'fileInput'
       ) as HTMLInputElement;
       const file = fileInput?.files?.[0];
-           //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
+      //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       await uploadFile(file, id, isAddingMode);
     }
   };
 
   const handleFileInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
     const file = e.target.files?.[0] || null;
-           //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
+    //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     uploadFile(file, id, isAddingMode);
   };
 
-    const handleDelete = () => {
+  const handleDelete = async () => {
     if (editedLaw && editedLaw.id) {
-      const productId = editedLaw.id;
-      dispatch(deleteProduct(productId));
+      const lawId = editedLaw.id;
+      await dispatch(deleteLaw(lawId));
       onCloseEditEditor();
     }
   };
@@ -356,8 +137,32 @@ const Editor: FC<LawEditorProps> = ({
           <div className="flex space-x-2 items-center justify-between pb-4">
             <div className="flex space-x-2">
               <h1 className="text-slate-600 text-sm tracking-normal leading-tight">
-                Новый
+                {isAddingMode ? 'Новый' : 'Редактирование'}
               </h1>
+              <div>
+                <input
+                  onChange={(e) =>
+                    setEditedLaw({ ...editedLaw, title: e.target.value })
+                  }
+                  id="title"
+                  type="text"
+                  value={editedLaw.title} 
+                  placeholder=""
+                  className="block py-2.5 px-0 w-full text-sm text-slate-500 bg-transparent border-0 border-b-2 border-slate-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-400 peer focus:text-green-500"
+                  required={true}
+                />
+                <input
+                  onChange={(e) =>
+                    setEditedLaw({ ...editedLaw, dateFrom: e.target.value })
+                  }
+                  id="dateFrom"
+                  type="date"
+                  value={editedLaw.dateFrom} 
+                  placeholder=""
+                  className="block py-2.5 px-0 w-full text-sm text-slate-500 bg-transparent border-0 border-b-2 border-slate-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-400 peer focus:text-green-500"
+                  required={true}
+                />
+              </div>
               <ReactQuill
                 theme="snow"
                 value={editedLaw.description}
@@ -366,6 +171,21 @@ const Editor: FC<LawEditorProps> = ({
                 }
                 className="w-full"
               />
+            </div>
+            <div className="flex space-x-2">
+              <button
+                type="button"
+                className="bg-red-500 text-white px-4 py-2 rounded"
+                onClick={handleDelete}
+              >
+                Удалить
+              </button>
+              <button
+                type="submit"
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                Далее
+              </button>
             </div>
           </div>
         )}
@@ -378,7 +198,8 @@ const Editor: FC<LawEditorProps> = ({
                   Форма загрузки документа
                 </h1>
                 <span className="block mt-2 text-sm text-gray-500">
-                  Если документ менять не нужно, вы можете пропустить этот шаг
+                  Если документ загружать не нужно, вы можете пропустить этот
+                  шаг
                 </span>
                 <div className="mt-6">
                   <div className="mb-4">
