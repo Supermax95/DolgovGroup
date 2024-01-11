d93e332ab84e134bf4aac1720e74ae1d1c85ee19
AleArtiu <rbirbrty0@gmail.com>
2024-01-11 18:19:25 +0300

    изменена форма загрузки фото в продуктах

--- Server/db/seeders/20231204092903-Promotions.js
+++ Server/db/seeders/20231204092903-Promotions.js
@@ -6,7 +6,7 @@ module.exports = {
         {
           title: 'Акция 1',
           description: 'Описание акции 1',
-          photo: 'ссылка_на_фото_1',
+          photo: null,
           dateStart: '2023-01-01',
           dateEnd: '2023-01-15',
           carousel: true,
@@ -17,7 +17,7 @@ module.exports = {
         {
           title: 'Акция 2',
           description: 'Описание акции 2',
-          photo: 'ссылка_на_фото_2',
+          photo: null,
           dateStart: '2023-02-01',
           dateEnd: '2023-02-28',
           carousel: false,
