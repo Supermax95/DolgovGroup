2282d1b744b0b06a25374125c4e24cdae6878297
AleArtiu <rbirbrty0@gmail.com>
2024-02-15 18:20:18 +0300

    fix email

--- App/src/components/UserProfile/EditProfile/ChangeEmail/ChangeEmail.tsx
+++ App/src/components/UserProfile/EditProfile/ChangeEmail/ChangeEmail.tsx
@@ -116,7 +116,7 @@ const ChangeEmail: FC = () => {
             </Text>
           )}
 
-          {newEmailProfile !== '' ? (
+          {newEmailProfile ? (
             <View className="w-full justify-center mt-1 px-1">
               <Text className="text-xs font-molmal text-rose-500">
                 Новая почта не подтверждена {newEmailProfile}
