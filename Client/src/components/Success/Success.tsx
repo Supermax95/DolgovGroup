a35e295f736848373724373eb7ce683d97e8b6f0
Supermax95 <wallmax@mail.ru>
2024-03-25 15:52:29 +0700

    front

--- Client/src/components/Success/Success.tsx
+++ Client/src/components/Success/Success.tsx
@@ -6,7 +6,7 @@ const Success: React.FC = () => {
       <div className="bg-white max-w-md p-8 rounded shadow-md text-center">
         <h2 className="text-2xl font-bold mb-6">Аккаунт активирован!</h2>
         <p className="text-gray-700 mb-4">
-          Теперь вы можете вернуться в приложение.
+        Выйдете из браузера и вернитесь в приложение "Наш Продукт".
         </p>
         <div className="flex justify-end"></div>
       </div>
