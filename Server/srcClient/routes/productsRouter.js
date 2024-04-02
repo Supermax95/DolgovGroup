47e6801c1a8ecf4ce8db0264aef05df0346c6901
Supermax95 <wallmax@mail.ru>
2024-04-02 14:55:43 +0700

    checkTask

--- Server/srcClient/routes/productsRouter.js
+++ Server/srcClient/routes/productsRouter.js
@@ -59,7 +59,7 @@ router.get('/admin/products', async (req, res) => {
   }
 });
 
-const task = cron.schedule('05 10 * * *', async () => {
+const task = cron.schedule('00 10 * * *', async () => {
   console.log('я в task=======>');
   try {
     const products = await Product.findAll({
