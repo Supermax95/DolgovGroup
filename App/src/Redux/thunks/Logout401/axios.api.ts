60e3d8314a23de94cedd2e4d282978ea3bdc235e
Supermax95 <wallmax@mail.ru>
2024-04-03 20:53:10 +0700

    fixPush

--- App/src/Redux/thunks/Logout401/axios.api.ts
+++ App/src/Redux/thunks/Logout401/axios.api.ts
@@ -32,3 +32,40 @@ function setupInterceptors(token: string, dispatch: any) {
 }
 
 export { axiosInstance, setupInterceptors };
+
+
+// import axios from 'axios';
+// import { userLogout } from '../User/logout.api';
+
+// const axiosInstance = axios.create();
+
+// function setupInterceptors(token: string, dispatch: any) {
+//   axiosInstance.interceptors.request.use(
+//     (config) => {
+//       if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//         config.headers['Content-Type'] = 'application/json';
+//       }
+//       return config;
+//     },
+//     (error) => {
+//       return Promise.reject(error);
+//     }
+//   );
+
+//   axiosInstance.interceptors.response.use(
+//     (response) => {
+//       return response;
+//     },
+//     async (error) => {
+//       if (error.response && error.response.status === 401) {
+//         if (token) {
+//           await dispatch(userLogout({ token }));
+//         }
+//       }
+//       return Promise.reject(error);
+//     }
+//   );
+// }
+
+// export { axiosInstance, setupInterceptors };
