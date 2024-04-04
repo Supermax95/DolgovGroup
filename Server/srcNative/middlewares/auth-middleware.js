61b1f333b9c1af181d6a7eff204df9bc3680423a
Supermax95 <wallmax@mail.ru>
2024-04-04 10:52:09 +0700

    tokeLow

--- Server/srcNative/middlewares/auth-middleware.js
+++ Server/srcNative/middlewares/auth-middleware.js
@@ -4,15 +4,16 @@ const tokenService = require('../services/token-service');
 const authMiddleware = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
+    console.log('authMiddlewareToken====>', token);
     if (!token) {
       return next(ApiError.UnauthorizedError());
     }
     const userData = tokenService.validateRefreshToken(token);
-
+console.log('userData========>', userData);
     if (!userData) {
       return next(ApiError.UnauthorizedError());
     }
-    // req.user = userData;
+    req.user = userData;
     next();
   } catch (e) {
     return next(ApiError.UnauthorizedError());
--- Server/srcNative/services/token-service.js
+++ Server/srcNative/services/token-service.js
@@ -3,10 +3,9 @@ const { Token } = require('../../db/models');
 
 class TokenService {
   generateTokens(payload) {
-
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
       // expiresIn: '15d',
-            expiresIn: '20s',
+      expiresIn: '10s',
     });
     console.log('==========>', refreshToken);
     return {
