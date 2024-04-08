23c8fdd2cdcfebe56671440f11302d4c23aab24e
AleArtiu <rbirbrty0@gmail.com>
2024-04-08 14:33:33 +0300

    update router multer product

--- Server/srcClient/routes/multerRouter.js
+++ Server/srcClient/routes/multerRouter.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const multer = require('multer');
+const path = require('path');
+const fsPromises = require('fs').promises;
 const { Product, Promotion, Law, Category } = require('../../db/models');
 const checkUser = require('../middlewares/auth-middleware-client');
 
@@ -50,20 +52,52 @@ router.put(
   checkUser,
   async (req, res) => {
     const { id } = req.params;
+
     const originalname = req.file.filename;
+
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ error: 'Продукт не найден' });
+    }
+
     try {
-      const product = await Product.findByPk(id);
+      const newPhotoPath = `/uploads/product/${originalname}`;
 
-      if (!product) {
-        res.status(404).json({ error: 'Продукт не найден' });
-      } else {
-        await product.update(
-          { photo: `/uploads/product/${originalname}` },
-          { where: { id } }
-        );
+      // Проверяем, существует ли уже файл с таким путем в базе данных
+      const existingProduct = await Product.findOne({
+        where: { photo: newPhotoPath },
+      });
 
-        res.json({ message: 'Файл загрузился.' });
+      if (existingProduct) {
+        return res
+          .status(400)
+          .json({ error: 'Файл с таким именем уже существует' });
       }
+
+      // Создаем путь к файлу
+      const filePath = path.join(__dirname, '..', '..', product.photo);
+
+      // Проверяем существование файла
+      const fileExists = await fsPromises
+        .access(filePath)
+        .then(() => true)
+        .catch(() => false);
+
+      if (fileExists && product.photo !== '/uploads/noPhoto/null.png') {
+        // Удаляем файл, только если путь к нему не равен /uploads/noPhoto/null.png
+        await fsPromises.unlink(filePath);
+        console.log(`Файл ${filePath} успешно удален`);
+      } else {
+        console.log(
+          `Файл ${filePath} не существует или его удаление не требуется`
+        );
+      }
+
+      // Обновляем путь к файлу в базе данных
+      await product.update({ photo: newPhotoPath }, { where: { id } });
+
+      res.json({ message: 'Файл загрузился.' });
     } catch (error) {
       console.log('error', error);
       res.status(500).json({ error: 'Ошибка загрузки' });
