705a4f4b7fe773b2a76b3b3cd86b3cabed2d8cdc
Supermax95 <wallmax@mail.ru>
2024-04-11 12:52:36 +0700

    fx

--- App/src/Redux/thunks/User/check.api.ts
+++ App/src/Redux/thunks/User/check.api.ts
@@ -1,7 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import type { AxiosResponse } from 'axios';
-import axios from 'axios';
+// import axios from 'axios';
 import { EXPO_PUBLIC_PORT, EXPO_PUBLIC_IP } from '@env';
+import { axiosInstance } from '../Logout401/axios.api';
+
 
 interface ICheckRequest {
   token?: string | undefined;
@@ -27,7 +29,7 @@ const getCheck = createAsyncThunk<
   }
 >('api/checkUser', async ({ token }, { rejectWithValue }) => {
   try {
-    const response: AxiosResponse<ICheckResponse> = await axios.get(
+    const response: AxiosResponse<ICheckResponse> = await axiosInstance.get(
       `http://${EXPO_PUBLIC_IP}:${EXPO_PUBLIC_PORT}/checkUser`,
       {
         headers: {
