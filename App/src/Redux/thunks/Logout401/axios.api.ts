47520aa7d853a24c9d9fa66a0df897ea9e3c10b2
Supermax95 <wallmax@mail.ru>
2024-04-11 12:51:21 +0700

    fx

--- App/src/Redux/thunks/Logout401/axios.api.ts
+++ App/src/Redux/thunks/Logout401/axios.api.ts
@@ -33,21 +33,24 @@
 
 // export { axiosInstance, setupInterceptors };
 
-
 import axios from 'axios';
 import { userLogout } from '../User/logout.api';
 
 const axiosInstance = axios.create();
 
 function setupInterceptors(token: string, dispatch: any) {
-  axiosInstance.interceptors.request.use
+  axiosInstance.interceptors.request.use;
 
   axiosInstance.interceptors.response.use(
     (response) => {
       return response;
     },
     async (error) => {
-      if (error.response && error.response.status === 401) {
+      if (
+        error.response &&
+        error.response.status === 401 &&
+        error.response.status === 500
+      ) {
         if (token) {
           await dispatch(userLogout({ token }));
         }
